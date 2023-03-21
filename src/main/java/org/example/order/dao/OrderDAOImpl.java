97ea390787c9ac925aed926a12db062acf9319dc
Burhanuddin Bhopalwala <bbhopalw+github@gmail.com>
2023-03-21 11:11:41 +0400

    junit 4 order service example with Mockito

---
+++ src/main/java/org/example/order/dao/OrderDAOImpl.java
@@ -0,0 +1,34 @@
+package org.example.order.dao;
+
+import com.bharath.order.dto.Order;
+
+import java.sql.SQLException;
+
+public class OrderDAOImpl implements OrderDAO {
+
+    @Override
+    public int create(Order order) throws SQLException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public Order read(int id) throws SQLException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int update(Order order) throws SQLException {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int delete(int id) throws SQLException {
+        // TODO Auto-generated method stub
+        return 0;
+
+    }
+
+}
